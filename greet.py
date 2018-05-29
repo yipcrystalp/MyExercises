6f6e8eae0f532e08c88dd5152ad706c586fd0921
yipcrystalp <34087128+yipcrystalp@users.noreply.github.com>
2018-05-29 14:18:02 +0200

    Tech Basics 1
    Exercise 1

---
+++ greet.py
@@ -0,0 +1,13 @@
+
+
+#define 'greet()'
+
+def greet():
+    print ("Hey there!")
+    name = raw_input("Please type your name: ")
+    print ("Nice to meet you " + name)
+    country = raw_input("So where are you from? ")
+    print ("Interesting, so you're from " + country)
+    print ("I'd love to visit there someday! :)")
+
+greet();
