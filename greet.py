f499df6f8c04fcd88488a0cded1b098b4bcc6006
yipcrystalp <34087128+yipcrystalp@users.noreply.github.com>
2018-05-29 14:52:55 +0200

    Tech Basics 1: Write a function
    29 May 2018

--- greet.py
+++ greet.py
@@ -1,7 +1,6 @@
 
 
 #define 'greet()'
-
 def greet():
     print ("Hey there!")
     name = raw_input("Please type your name: ")
@@ -9,5 +8,15 @@ def greet():
     country = raw_input("So where are you from? ")
     print ("Interesting, so you're from " + country)
     print ("I'd love to visit there someday! :)")
+
+
+    choice = raw_input("Can I sing a song for you? yes or no? ")
+
+    if choice == "yes":
+        s = open("birds.txt", "r")
+        song = s.read()
+        print song
+    else:
+        print("Okay, no problem, goodbye! :)")
 
 greet();
